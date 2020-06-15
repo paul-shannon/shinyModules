9a6fc339f38bdf2161960ef145be62bac1b439a7
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-06-15 10:14:13 -0700

    very simple

---
+++ messageBox/demoApp.R
@@ -0,0 +1,28 @@
+library(shiny)
+source("messageBoxModule.R")
+#----------------------------------------------------------------------------------------------------
+ui <- fluidPage(
+        messageBoxUI(id="messageBox.1", title="box 1", titleSpan=2, boxSpan=10),
+        messageBoxUI(id="messageBox.2", title="box 2", titleSpan=1, boxSpan=8),
+        actionButton("randomTextButton", label= "Generate random Text",
+                     style="margin-top: 40px; margin-left: 200px;")
+        )
+
+#----------------------------------------------------------------------------------------------------
+server <- function(input, output, session)
+{
+
+  randomText <- reactiveVal("")
+
+  messageBox1.contents <- callModule(messageBoxServer, "messageBox.1",
+                                     newContent=randomText)
+  messageBox2.contents <- callModule(messageBoxServer, "messageBox.2",
+                                     newContent=reactive(tolower(randomText())))
+
+  observeEvent(input$randomTextButton, ignoreInit=TRUE, {
+     randomText(paste(sample(c(LETTERS, letters), 10, replace=TRUE), collapse=""))
+     })
+
+} # server
+#----------------------------------------------------------------------------------------------------
+runApp(shinyApp(ui, server), port=9012)
---
+++ messageBox/messageBoxModule.R
@@ -0,0 +1,15 @@
+library(shiny)
+
+messageBoxUI <- function(id, title, titleSpan=1, boxSpan=11){
+   fluidRow(
+      if(titleSpan > 0) column(titleSpan, span(title)),
+      column(boxSpan, verbatimTextOutput(NS(id, "messageBox")))
+      )
+   }
+
+messageBoxServer <- function(input, output, session, newContent){
+  output$messageBox <- renderPrint(newContent())
+  }
+
+
+
