c4360fa55d299d923756c3aa97c97a184fc81c71
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-07-10 07:50:45 -0700

    first tentative use of insertUi

--- inst/unitTests/ExperimentalMeasuresDemoApp.R
+++ inst/unitTests/ExperimentalMeasuresDemoApp.R
@@ -5,10 +5,11 @@ tbl <- get(load(system.file(package="shinyModules", "extdata",
 analytes <- sort(unique(tbl$analyte))
 aoi.01 <- "LMNB1_pS23_AGGP"
 exoi.01 <- "DMSO"
-tbl.sub.01 <- subset(tbl, analyte==aoi.01 & groupName==exoi.01)  # has good distribution of area
+coi <- c("experiment", "time", "radiation", "area", "sd")
+tbl.sub.01 <- subset(tbl, analyte==aoi.01 & groupName==exoi.01)[, coi]
 
 exoi.02 <- "ATRi -/+"
-tbl.sub.02 <- subset(tbl, analyte==aoi.01 & groupName==exoi.02)  # has good distribution of area
+tbl.sub.02 <- subset(tbl, analyte==aoi.01 & groupName==exoi.02)[, coi]
 
 #----------------------------------------------------------------------------------------------------
 ui.simple <- fluidPage(
@@ -21,27 +22,55 @@ ui.simple <- fluidPage(
 #----------------------------------------------------------------------------------------------------
 ui.dashboard <- dashboardPage(
 
-  dashboardHeader(title = "Dynamic sidebar"),
+  dashboardHeader(title = "Proteomic Assays"),
   dashboardSidebar(
+      selectInput("selectAnalyte",
+                  label="Analyte",
+                  c("CDC25B_pS160_LLGH", "LMNB1_pS23_AGGP", "MDM2_pS166_AISE", "TP53_pS315_ALPN")
+                  ),
     sidebarMenuOutput("menu")
     ),
   dashboardBody(
-      fluidRow(
-          box(ExperimentalMeasuresUI(id="expMeasures.01", title=sprintf("%s: %s", aoi.01, exoi.01)),
-              title=sprintf("%s - %s", aoi.01, exoi.01)),
-          box(ExperimentalMeasuresUI(id="expMeasures.02", title=sprintf("%s: %s", aoi.01, exoi.02)),
-              title=sprintf("%s - %s", aoi.01, exoi.02))
-         )
+      uiOutput("plotBoxDiv")  # creates a div to be filled later
+
+      #fluidRow(
+          #box(ExperimentalMeasuresUI(id="expMeasures.01", title=sprintf("%s: %s", aoi.01, exoi.01)),
+          #    title=sprintf("%s - %s", aoi.01, exoi.01)),
+          #box(ExperimentalMeasuresUI(id="expMeasures.02", title=sprintf("%s: %s", aoi.01, exoi.02)),
+          #    title=sprintf("%s - %s", aoi.01, exoi.02))
+       #  )
     )
 
 ) # ui.dashboard
 #----------------------------------------------------------------------------------------------------
 server <- function(input, output, session)
 {
-  callModule(ExperimentalMeasuresServer, "expMeasures.01", tbl.sub.01)
-  callModule(ExperimentalMeasuresServer, "expMeasures.02", tbl.sub.02)
+    observeEvent(input$selectAnalyte, ignoreInit=TRUE,{
+       analyte <- input$selectAnalyte;
+       printf("new analyte: %s", analyte)
+       insertUI("#plotBoxDiv", "afterBegin",
+                ui=h5("hello plotBoxDiv"))
+       #displayAnalyteDataByExperiment(analyte)
+       })
+
+  #callModule(ExperimentalMeasuresServer, "expMeasures.01", tbl.sub.01)
+  #callModule(ExperimentalMeasuresServer, "expMeasures.02", tbl.sub.02)
 
 } # server
+#----------------------------------------------------------------------------------------------------
+displayAnalyteDataByExperiment <- function(analyte.name)
+{
+   coi <- c("experiment", "time", "radiation", "area", "sd")
+
+   tbl.sub <- subset(tbl, analyte==analyte.name)
+   experiments <- unique(tbl.sub$groupName)
+
+   for(experiment in experiments){
+      tbl.exp <- subset(tbl.sub, groupName==experiment)[, coi]
+      callModule(ExperimentalMeasuresServer, experiment, tbl.exp)
+      }
+
+} # displayAnalyteDataByExperiment
 #----------------------------------------------------------------------------------------------------
 runApp(shinyApp(ui.dashboard, server), port=9082)
 #runApp(shinyApp(ui.simple, server), port=9082)
