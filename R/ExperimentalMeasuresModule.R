d87ebb3b2f4d61031086192df4542edd38493eb7
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-08-12 07:05:25 -0700

    ExperimentalMeasures has new ggplot call suggested by Jeff

--- R/ExperimentalMeasuresModule.R
+++ R/ExperimentalMeasuresModule.R
@@ -41,19 +41,28 @@ ExperimentalMeasuresUI <- function(id, title, boxHeight=300, boxWidth=320, fontS
 ExperimentalMeasuresServer <- function(id, tbl) {
 
     moduleServer(id, function(input, output, session){
-        # printf("--- executing ExperimentalMeasuresServer, id: %s", id)
         doNotKnowWhyThisIsNeeded <- tbl    # a bug in the new shiny 1.5.0? (11 jul 2020)
         output$dataTable <- renderTable(tbl)
 
-        yMax <- max(tbl$area) * 1.1
-        printf("EMM.R, yMax: %f", yMax)
-        if (yMax < 6)
-           yMax <- 6
-
+           # preserve the incoming order of the data.frame
+        tbl$variable <- factor(tbl$variable, levels=tbl$variable)
         output$barplot <- renderPlot({
-            tbl$x.axis.value <- seq_len(nrow(tbl))
-            ggplot(data=tbl, aes(x=x.axis.value, y=area)) + geom_bar(stat='identity') + ylim(0, yMax) + xlab("time or condition")
+            ggplot(tbl, aes(x=variable, y=mean, fill=variable)) +
+                geom_bar(position="dodge", stat="identity") +
+                geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), width=.4, position=position_dodge(.9)) +
+                #ggtitle(title)
+                scale_fill_manual(values=tbl$color) +
+                theme(text = element_text(size=20, face="bold"),
+                      axis.text.x = element_blank(), # element_text(angle = 45, hjust = 1.2, vjust=1.2),
+                      plot.title = element_text(color="navajowhite4", size=20, face="bold.italic", hjust=0.5),
+                      axis.title.x =  element_blank(),
+                      axis.title.y = element_text(color="dimgray", size=20, face="bold"),
+                      legend.text = element_text(colour="black", size=10, face="bold"))
             })
+        #output$barplot <- renderPlot({
+        #    tbl$x.axis.value <- seq_len(nrow(tbl))
+        #    ggplot(data=tbl, aes(x=x.axis.value, y=area)) + geom_bar(stat='identity') + ylim(0, yMax) + xlab("time or condition")
+        #    })
         }) # moduleServer
 
     } # ExperimentalMeasuresServer
